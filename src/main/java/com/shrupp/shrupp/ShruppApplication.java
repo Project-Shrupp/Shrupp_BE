dcc2413495b25a45e6ce35936c259dd87dc3bd34
rt3310 <btac3310@gmail.com>
2023-03-06 00:27:10 +0900

    🎉 create project

---
+++ src/main/java/com/shrupp/shrupp/ShruppApplication.java
@@ -0,0 +1,13 @@
+package com.shrupp.shrupp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShruppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShruppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrupp/shrupp/ShruppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrupp.shrupp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShruppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
