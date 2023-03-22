2088a47d85e8416a23cc27e7f3d948467e9761bd
rt3310 <btac3310@gmail.com>
2023-03-22 23:22:35 +0900

    feature: 게시글 좋아요 기능 구현

---
+++ src/main/java/com/shrupp/shrupp/domain/post/service/PostLikeService.java
@@ -0,0 +1,46 @@
+package com.shrupp.shrupp.domain.post.service;
+
+import com.shrupp.shrupp.domain.member.domain.Member;
+import com.shrupp.shrupp.domain.member.service.MemberService;
+import com.shrupp.shrupp.domain.post.domain.Post;
+import com.shrupp.shrupp.domain.post.domain.PostLike;
+import com.shrupp.shrupp.domain.post.dto.request.PostLikeRequest;
+import com.shrupp.shrupp.domain.post.repository.PostLikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostLikeService {
+
+    private final PostLikeRepository postLikeRepository;
+    private final PostService postService;
+    private final MemberService memberService;
+
+    public boolean like(Long postId, PostLikeRequest postLikeRequest) {
+        Post post = postService.findById(postId);
+        Member member = memberService.findById(postLikeRequest.memberId());
+
+        if (postLikeRepository.findPostLikeByPostAndMember(post, member).isPresent()) {
+            return false;
+        }
+
+        postLikeRepository.save(new PostLike(post, member));
+        return true;
+    }
+
+    public boolean unlike(Long postId, PostLikeRequest postLikeRequest) {
+        Post post = postService.findById(postId);
+        Member member = memberService.findById(postLikeRequest.memberId());
+
+        Optional<PostLike> postLike = postLikeRepository.findPostLikeByPostAndMember(post, member);
+        if (postLike.isEmpty()) {
+            return false;
+        }
+
+        postLikeRepository.delete(postLike.get());
+        return true;
+    }
+}
