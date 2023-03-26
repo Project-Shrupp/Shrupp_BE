218422ad1e6ea3c57e7e96cac793a84e563134e2
rt3310 <btac3310@gmail.com>
2023-03-26 23:43:23 +0900

    fix: 카카오 닉네임 파싱 오류 수정

--- src/main/java/com/shrupp/shrupp/config/security/oauth2/mapper/KakaoAttributeMapper.java
+++ src/main/java/com/shrupp/shrupp/config/security/oauth2/mapper/KakaoAttributeMapper.java
@@ -5,15 +5,15 @@ import com.shrupp.shrupp.config.security.oauth2.OAuth2Request;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.Objects;
 
 @Component
 public class KakaoAttributeMapper implements AttributeMapper {
     @Override
     public OAuth2Request mapToDto(Map<String, Object> attributes) {
         String accountId = (attributes.get("id")).toString();
-        String name = (String) attributes.get("name");
-        String email = (String) attributes.get("email");
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        String name = (String) ((Map<String, Object>) kakaoAccount.get("profile")).get("nickname");
+        String email = (String) kakaoAccount.get("email");
         return new OAuth2Request(accountId, name, email, AuthProvider.KAKAO);
     }
 }
