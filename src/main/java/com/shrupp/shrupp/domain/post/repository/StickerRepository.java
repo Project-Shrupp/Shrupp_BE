825cd7ff7574eeff2a52da812d879e7eb77ac586
rt3310 <btac3310@gmail.com>
2023-04-23 19:07:30 +0900

    fix: OSIV OFF 설정으로 인한 session 오류 및 N+1 문제 수정

--- src/main/java/com/shrupp/shrupp/domain/post/repository/StickerRepository.java
+++ src/main/java/com/shrupp/shrupp/domain/post/repository/StickerRepository.java
@@ -2,7 +2,12 @@ package com.shrupp.shrupp.domain.post.repository;
 
 import com.shrupp.shrupp.domain.post.domain.Sticker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StickerRepository extends JpaRepository<Sticker, Long> {
 
+    @Query("select s from Sticker s left join fetch s.member")
+    List<Sticker> findAllFetchWithMember();
 }
