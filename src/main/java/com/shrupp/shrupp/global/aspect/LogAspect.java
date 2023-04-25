b5a95888e6a552b3e10b8a09edd80adf4959342c
rt3310 <btac3310@gmail.com>
2023-04-25 13:18:24 +0900

    feature: controller logging 구현 by aspect

---
+++ src/main/java/com/shrupp/shrupp/global/aspect/LogAspect.java
@@ -0,0 +1,45 @@
+package com.shrupp.shrupp.global.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+    @Pointcut("execution(* com.shrupp.shrupp..*Controller.*(..))")
+    public void allController() { }
+
+    @Around("allController()")
+    public Object createControllerLog(ProceedingJoinPoint joinPoint) {
+        log.info("[REQUEST] Controller: {} | Method: {} | Arguments: {}",
+                joinPoint.getTarget().getClass().getSimpleName(),
+                joinPoint.getSignature().toShortString(),
+                toParametersString(
+                        ((CodeSignature) (joinPoint.getSignature())).getParameterNames(),
+                        joinPoint.getArgs()));
+
+        try {
+            Object result = joinPoint.proceed();
+            log.info("[REQUEST] Result: {}", result.getClass().getSimpleName());
+            return result;
+        } catch (Throwable e) {
+            return null;
+        }
+    }
+
+    private String toParametersString(String[] parameterNames, Object[] args) {
+        return IntStream.range(0, parameterNames.length)
+                .mapToObj(i -> parameterNames[i] + "=" + args[i])
+                .collect(Collectors.joining(","));
+    }
+}
