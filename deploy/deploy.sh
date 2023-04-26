44282bcf25891d6e4ebad9bebf25ae2f7632160c
rt3310 <btac3310@gmail.com>
2023-04-26 14:12:41 +0900

    chore: 빌드, 배포 파일 및 환경 구성

---
+++ deploy/deploy.sh
@@ -0,0 +1,10 @@
+REPOSITORY=/home/ec2-user/
+
+cd $REPOSITORY
+
+docker stop shrupp
+docker rm shrupp
+docker rmi shrupp-image
+
+docker build -t shrupp-image .
+docker run -d -p 8080:8080 --name shrupp shrupp-image
