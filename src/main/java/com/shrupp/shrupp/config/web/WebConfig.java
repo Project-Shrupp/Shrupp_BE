ca3879a9810c4b159c3376108248bb45205da70c
rt3310 <btac3310@gmail.com>
2023-05-06 00:03:06 +0900

    hotfix: webConfigurer 설정 추가

---
+++ src/main/java/com/shrupp/shrupp/config/web/WebConfig.java
@@ -0,0 +1,23 @@
+package com.shrupp.shrupp.config.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${client.url}")
+    private String clientUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(clientUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
