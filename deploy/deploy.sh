4c9818afdeee0ce47d54f8b439fda045e706cb5f
rt3310 <btac3310@gmail.com>
2023-05-06 21:58:51 +0900

    fix: docker port mapping 변경

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,4 +7,4 @@ docker rm shrupp
 docker rmi shrupp-image
 
 docker build -t shrupp-image .
-docker run -d -p 8080:8080 --name shrupp shrupp-image
+docker run -d -p 443:8080 --name shrupp shrupp-image
