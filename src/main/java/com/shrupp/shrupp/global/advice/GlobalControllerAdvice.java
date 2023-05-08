857e464135fe5ea2c7671d5b7afeb6e4b6190524
rt3310 <btac3310@gmail.com>
2023-05-08 22:16:00 +0900

    feature: controllerAdvice 로깅 로직 추가

--- src/main/java/com/shrupp/shrupp/global/advice/GlobalControllerAdvice.java
+++ src/main/java/com/shrupp/shrupp/global/advice/GlobalControllerAdvice.java
@@ -1,21 +1,31 @@
 package com.shrupp.shrupp.global.advice;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> entityNotFound(EntityNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
+    public ResponseEntity<ErrorResponse> entityNotFoundHandler(EntityNotFoundException e) {
+        log.info("EntityNotFoundException: {}", e.getMessage());
+        return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> runtimeException(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+    public ResponseEntity<ErrorResponse> runtimeExceptionHandler(RuntimeException e) {
+        log.info("RuntimeException: {}", e.getMessage());
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+        log.info("Exception: {}", e.getMessage());
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 }
