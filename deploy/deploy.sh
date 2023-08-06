cfcd403c4d92843c849f2032397326cf8d775ff0
rt3310 <btac3310@gmail.com>
2023-08-06 12:17:00 +0900

    chore: deploy shell script 수정

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,5 +6,10 @@ docker stop shrupp
 docker rm shrupp
 docker rmi shrupp-image
 
-docker build -t shrupp-image . --build-arg DB_USERNAME=$DB_USERNAME --build-arg DB_PASSWORD=$DB_PASSWORD
+docker build -t shrupp-image .\
+--build-arg DB_USERNAME=$DB_USERNAME\
+--build-arg DB_PASSWORD=$DB_PASSWORD\
+--build-arg DKAKAO_CLIENT_ID=$DKAKAO_CLIENT_ID\
+--build-arg DKAKAO_CLIENT_SECRET=$DKAKAO_CLIENT_SECRET
+
 docker run -d -p 8080:8080 --name shrupp shrupp-image
